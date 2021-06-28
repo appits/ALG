cb5f4b019f4c708fdb4aaf4bd2d31f638178c12b
oscar <oscar.llovera90@gmail.com>
2021-06-28 12:44:39 -0400

    Modulo para registro de activos depreciados.

---
+++ account_asset_depreciated/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_asset_depreciated",
+    'summary': """
+        Recording of depreciated assets.
+    """,
+    'author': "Oscar Llovera",
+    'category': 'Accounting/Accounting',
+    'version': '0.1',
+    'depends': ['account_asset'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+}
