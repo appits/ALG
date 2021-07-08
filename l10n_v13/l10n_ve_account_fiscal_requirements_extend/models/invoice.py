c37fac37a1c18b22dda371fc5309cb24f49e7ed2
oscar <oscar.llovera90@gmail.com>
2021-07-08 17:55:33 -0400

    Fix validaciones fiscales factura rectificativas.

--- l10n_v13/l10n_ve_account_fiscal_requirements_extend/models/invoice.py
+++ l10n_v13/l10n_ve_account_fiscal_requirements_extend/models/invoice.py
@@ -12,11 +12,21 @@ class Invoice(models.Model):
         if self.maq_fiscal_p:
             self.nro_ctrl = '0'
         else:
-            self.nro_ctrl = ''
+            super().onchange_maquina_fiscal()
     
     @api.constrains('maq_fiscal_p')
     def _check_maq_fiscal_p(self):
         for invoice in self:
-            if invoice.maq_fiscal_p and not invoice.nro_ctrl:
+            types = ('out_refund', 'in_refund')
+            if invoice.maq_fiscal_p and not invoice.nro_ctrl and invoice.type not in types:
                 raise ValidationError('Debe ingresar un numero de control.')
+    
+    @api.model
+    def create(self, values):
+        invoice = super().create(values)
+
+        if invoice.maq_fiscal_p and not invoice.nro_ctrl:
+            invoice.nro_ctrl = '0'
+        
+        return invoice
 
